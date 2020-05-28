c39b0b4dd29ef944e1204e605c7aa4b4a8aa2e4d
Abhishek <abhishekkhandelwal47@gmail.com>
2020-05-28 14:19:18 +0530

    third

--- tweak.py
+++ tweak.py
@@ -1,6 +1,6 @@
 import os
 
-acc = os.popen("cat /var/lib/jenkins/workspace/mlops_job1/accuracy.txt")
+acc = os.popen("cat /var/lib/jenkins/workspace/mlt-job1/accuracy.txt")
 acc1 = acc.read()
 print(acc1)
 acc2 = acc1.rstrip()
@@ -9,7 +9,7 @@ acc3 = float(acc2)
 
 
 if acc3<80:
-    x = os.popen("cat /var/lib/jenkins/workspace/mlops_job1/train.py | grep model.add | wc -l")
+    x = os.popen("cat /var/lib/jenkins/workspace/mlt-job1/dl.py | grep model.add | wc -l")
     x1 = x.read()
     x2 = x1.rstrip()
     x3 = int(x2)
@@ -24,7 +24,7 @@ if acc3<80:
         print("MAAF KARDO")
         exit()
     os.system("sed -i '/softmax/ i {}' /var/lib/jenkins/workspace/mlops_job1/train.py".format(y))
-    os.system("curl -u admin:redhat http://192.168.99.102:8080/view/Ml%20+%20Jenkins/job/mlops_job2/build?token=trainjob")
+    #os.system("curl -u admin:redhat http://192.168.99.102:8080/view/Ml%20+%20Jenkins/job/mlops_job2/build?token=trainjob")
     acc = os.popen("cat /var/lib/jenkins/workspace/mlops_job1/accuracy.txt")
     acc1 = acc.read()
     print(acc1)
@@ -32,5 +32,5 @@ if acc3<80:
     print(acc2)
     acc3 = float(acc2)
 else:
-    os.system("curl -u admin:redhat 192.168.99.102:8080/view/Ml%20+%20Jenkins/job/mlops_mail/build?token=mailing")
+    #os.system("curl -u admin:redhat 192.168.99.102:8080/view/Ml%20+%20Jenkins/job/mlops_mail/build?token=mailing")
     print("ACCURACY ABOVE 80")
