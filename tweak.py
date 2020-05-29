25272dc5a158f7fb0af2ccca046fe576c4ecd410
Abhishek <abhishekkhandelwal47@gmail.com>
2020-05-29 11:03:09 +0530

    update tweak

--- tweak.py
+++ tweak.py
@@ -1,6 +1,6 @@
 import os
 
-acc = os.popen("cat /var/lib/jenkins/workspace/mlt-job1/accuracy.txt")
+acc = os.popen("cat /root/Desktop/tasks/mlops/accuracy.txt")
 acc1 = acc.read()
 print(acc1)
 acc2 = acc1.rstrip()
@@ -9,7 +9,7 @@ acc3 = float(acc2)
 
 
 if acc3<80:
-    x = os.popen("cat /var/lib/jenkins/workspace/mlt-job1/dl.py | grep model.add | wc -l")
+    x = os.popen("cat /root/Desktop/tasks/mlops/dl.py | grep model.add | wc -l")
     x1 = x.read()
     x2 = x1.rstrip()
     x3 = int(x2)
@@ -23,9 +23,9 @@ if acc3<80:
     else:
         print("MAAF KARDO")
         exit()
-    os.system("sed -i '/softmax/ i {}' /var/lib/jenkins/workspace/mlt-job1/dl.py".format(y))
+    os.system("sed -i '/softmax/ i {}' /root/Desktop/tasks/mlops/dl.py".format(y))
     #os.system("curl -u admin:redhat http://192.168.99.102:8080/view/Ml%20+%20Jenkins/job/mlops_job2/build?token=trainjob")
-    acc = os.popen("cat /var/lib/jenkins/workspace/mlt-job1/accuracy.txt")
+    acc = os.popen("cat /root/Desktop/tasks/mlops/accuracy.txt")
     acc1 = acc.read()
     print(acc1)
     acc2 = acc1.rstrip()
